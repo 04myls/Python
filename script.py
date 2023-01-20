6ee9ca383968bbea12ebe9e83a109499c8a842a7
M <117243176+04myls@users.noreply.github.com>
2023-01-20 11:15:01 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -1,2 +1,27 @@
 print('Hello, world!')
-print(1 + 2 + 3 + 4 + 5)
+
+# raw文字列
+print(R"年齢は22歳です")
+
+# 文字列と数値の連結
+num = 22
+print("My age is " + str(num))
+
+# 文字列の連結
+str1 = "04,"
+str2 = "myls"
+print(str1 + str2)
+
+# 文字数の取得
+print("04mylsの文字数は" +str(len("04myls")))
+
+# スライス
+str = "04myls"
+
+print(str[1:4])
+
+print(str[2:6])
+
+# スライス　省略
+print("04myls"[:3])
+
